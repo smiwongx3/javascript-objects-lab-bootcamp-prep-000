d89c963f8eb7fe7b43f8b24f802eb7ba76c21909
Samantha Wong <smiwong920@gmail.com>
2019-01-26 20:37:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   var newObject = Object.assign({}, object)
   delete newObject[key] 
+  return newObject
 }
