a515209a3e0e0d39ca0857ec65460ce0b6b0d8a7
Samantha Wong <smiwong920@gmail.com>
2019-01-26 20:33:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var recipes ={} 
 
-function updateObjectWithKeyAndValue (object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
